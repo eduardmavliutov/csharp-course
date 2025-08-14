476d1a2a9a9c65c07601bf3f5d92bc8a05172f78
Eduard Mavliutov <40761402+eduardmavliutov@users.noreply.github.com>
2025-08-14 10:18:34 +0300

    Number conversions task

--- DataTypes/Program.cs
+++ DataTypes/Program.cs
@@ -1,2 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Fix type conversion errors
+
+// var varValue = 100;
+// short shortValue;
+// sbyte sbyteValue;
+// ushort ushortValue;
+// long longValue;
+// longValue = varValue;
+// sbyteValue = varValue;
+// ushortValue = sbyteValue;
+// shortValue = ushortValue;
+
+var varValue = 100;
+short shortValue;
+sbyte sbyteValue;
+ushort ushortValue;
+long longValue;
+longValue = varValue;
+sbyteValue = (sbyte)varValue;
+ushortValue = (ushort)sbyteValue;
+shortValue = (short)ushortValue;
+
+// long longValue = 100;
+// var varValue = 100.0;
+// float floatValue;
+// double doubleValue;
+// decimal decimalValue;
+// longValue = varValue;
+// floatValue = varValue;
+// doubleValue = varValue;
+// decimalValue = varValue;
+// doubleValue = decimalValue;
+// decimalValue = longValue;
+
+long longValue = 100;
+var varValue = 100.0;
+float floatValue;
+double doubleValue;
+decimal decimalValue;
+
+longValue = (long)varValue;
+floatValue = (float)varValue;
+doubleValue = varValue;
+decimalValue = (decimal)varValue;
+doubleValue = (double)decimalValue;
+decimalValue = longValue;
+
+
